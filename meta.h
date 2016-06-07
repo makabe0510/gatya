78bb41de83db81fc2ddbf69f11b13214fad0bfc8
L76caspian <a.@examd.com>
2016-06-07 16:06:28 +0900

    Tekitou

---
+++ meta.h
@@ -0,0 +1,14 @@
+#define jewel8400 9800
+#define jewel4200 5000
+#define jewel2650 3200
+#define jewel1300 1600
+#define jewel760 960
+#define jewel360 480
+#define jewel60 120
+#define money9800 8400
+#define money5000 4200
+#define money3200 2650
+#define money1600 1300
+#define money960 760
+#define money480 360
+#define money120 60
